dec45d4f7e62fec46e45f802f5065b085ebb86ea
gchh <skring.2008@163.com>
2019-05-17 10:06:48 +0800

    Update 4.7.cpp

--- 4.7.cpp
+++ 4.7.cpp
@@ -348,3 +348,127 @@ int main()
 	cout << s1(5, 10) << endl;
 	return 0;
 }
+/******************************************使用string类：DEV C++/visual studio***********************************************/
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<cstdlib>
+using namespace std;
+class MyString{
+	string str;
+public:
+	MyString(){ }
+	MyString(const char * s)
+	{
+		str = s;
+	}
+	MyString(const MyString & s)
+	{
+		str = s.str;
+	}
+	MyString & operator=(const MyString & s)
+	{
+		str.assign(s.str, 0, s.str.length() + 1);
+		return *this;
+	}
+	MyString & operator=(const char * s)
+	{
+		str.assign(s);
+		return *this;
+	}
+	MyString operator+(const MyString & s)
+	{
+		MyString sTmp(*this);
+		sTmp.str += s.str;
+		return sTmp;
+	}
+	MyString operator+(const char * s)
+	{
+		MyString sTmp(*this);
+		sTmp.str += s;
+		return sTmp;
+	}
+	MyString & operator+=(const char * s)
+	{
+		str += s;
+		return *this;
+	}
+	friend MyString operator+(const char * s, const MyString & st);
+	friend ostream & operator<<(ostream & os, const MyString & s);
+	char & operator[](int i)
+	{
+		return str[i];
+	}
+	string operator()(int st, int sp)	
+	{
+		return str.substr(st, sp);
+	}
+	bool operator<(const MyString & s)
+	{
+		if (str.compare(s.str)<0) return true;
+		else return false;
+	}
+	bool operator==(const MyString & s)
+	{
+		if (str.compare(s.str)==0) return true;
+		else return false;
+	}
+	bool operator>(const MyString & s)
+	{
+		if (str.compare(s.str)>0) return true;
+		else return false;
+	}
+};
+MyString operator+(const char * s, const MyString & st)
+{
+	MyString ss;
+	ss.str.assign(s);
+	ss.str += st.str;
+	return ss;
+}
+ostream & operator<<(ostream & os, const MyString & s)
+{
+	os << s.str;
+	return os;
+}
+
+int CompareString(const void *e1, const void *e2)
+{
+	MyString *s1 = (MyString *)e1;
+	MyString *s2 = (MyString *)e2;
+	if (*s1 < *s2) return -1;
+	else if (*s1 == *s2) return 0;
+	else if (*s1 > *s2) return 1;
+}
+int main()
+{
+	MyString s1("abcd-"), s2, s3("efgh-"), s4(s1);
+	MyString SArray[4] = { "big", "me", "about", "take" };
+	cout << "1." << s1 << s2 << s3 << s4 << endl;
+	s4 = s3;
+	s3 = s1 + s3;
+	cout << "2." << s1 << endl;
+	cout << "3." << s2 << endl;
+	cout << "4." << s3 << endl;
+	cout << "5." << s4 << endl;
+	cout << "6." << s1[2] << endl;
+	s2 = s1;
+	s1 = "ijkl-";
+	s1[2] = 'A';
+	cout << "7." << s2 << endl;
+	cout << "8." << s1 << endl;
+	s1 += "mnop";
+	cout << "9." << s1 << endl;
+	s4 = "qrst-" + s2;
+	cout << "10." << s4 << endl;
+	s1 = s2 + s4 + "uvw" + "xyz";
+	cout << "11." << s1 << endl;
+	qsort(SArray, 4, sizeof(MyString), CompareString);
+	for (int i = 0; i < 4; ++i)
+		cout << SArray[i] << endl;
+	//输出s1从下标为0开始长度为4的字串
+	cout << s1(0, 4) << endl;
+	//输出s1从下标为5开始长度为10的字串
+	cout << s1(5, 10) << endl;
+	return 0;
+}
