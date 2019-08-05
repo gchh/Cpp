e402fe0039746bc4ea3b3a5ad4bedb29b721bbbe
gchh <skring.2008@163.com>
2019-08-05 17:20:30 +0800

    Create 014:MyString.c

---
+++ MOOC/014:MyString.c
@@ -0,0 +1,91 @@
+/***********************************************************************************************
+014:MyString
+总时间限制: 1000ms 内存限制: 65536kB 
+描述
+补足MyString类，使程序输出指定结果
+
+输入
+多组数据，每组一行，是两个不带空格的字符串
+
+输出
+对每组数据，先输出一行，打印输入中的第一个字符串三次
+然后再输出一行，打印输入中的第二个字符串三次
+
+样例输入
+abc def
+123 456
+
+样例输出
+abc,abc,abc
+def,def,def
+123,123,123
+456,456,456
+***********************************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+class MyString {
+	char * p;
+public:
+	MyString(const char * s) {
+		if( s) {
+			p = new char[strlen(s) + 1];
+			strcpy(p,s);
+		}
+		else
+			p = NULL;
+
+	}
+	~MyString() { if(p) delete [] p; }
+// 在此处补充你的代码  
+	void Copy(const char *s) 
+	{
+		if(p) delete p;
+		p = new char[strlen(s) + 1];
+		strcpy(p,s);
+	}
+	MyString & operator=(const MyString & s)
+	{
+		//if(strlen(p)<strlen(s.p))
+		//{
+		//	if(p) delete[] p;
+			p=new char[strlen(s.p)+1]; 
+		//}
+		strcpy(p,s.p);
+		return *this;
+	}
+	MyString & operator=(const char * s)
+	{
+		if(strlen(p)<strlen(s))
+		{
+			if(p) delete[] p;
+			p=new char[strlen(s)+1]; 
+		}
+		strcpy(p,s);
+		return *this;
+	}	
+	friend ostream & operator<<(ostream & os, const MyString &s)
+	{
+		os<<s.p;
+		return os;
+	}
+// 在此处补充你的代码
+};
+int main()
+{
+	char w1[200],w2[100];
+	while( cin >> w1 >> w2) {
+		MyString s1(w1),s2 = s1;
+		MyString s3(NULL);
+		s3.Copy(w1);
+		cout << s1 << "," << s2 << "," << s3 << endl;
+
+		s2 = w2;
+		s3 = s2;
+		s1 = s3;
+		cout << s1 << "," << s2 << "," << s3 << endl;
+		
+	}
+}
