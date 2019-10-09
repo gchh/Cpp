d136e42f10e89ce26f2f8790946fdb524662b4c1
gchh <skring.2008@163.com>
2019-10-09 15:19:20 +0800

    Update 031:山寨版istream_iterator.cpp

--- MOOC/031:山寨版istream_iterator.cpp
+++ MOOC/031:山寨版istream_iterator.cpp
@@ -73,4 +73,48 @@ int main() {
 	cout<<endl;
 	return 0; 
 }
-*********************************/
+*************************************************************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+template <class T>
+class CMyistream_iterator
+{
+	// 在此处补充你的代码
+	istream& in;
+	T value;
+public:
+	CMyistream_iterator(istream& i) :in(i){ cin >> value; }
+	T operator*(){ return value; }
+	CMyistream_iterator& operator++()
+	{
+		cin >> value;
+		return *this;
+	}
+	// 在此处补充你的代码
+};
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		CMyistream_iterator<int>inputInt(cin);
+		int n1, n2, n3;
+		n1 = *inputInt; //读入 n1
+		int tmp = *inputInt;
+		cout << tmp << endl;
+		inputInt++;
+		n2 = *inputInt; //读入 n2
+		inputInt++;
+		n3 = *inputInt; //读入 n3
+		cout << n1 << " " << n2 << " " << n3 << " ";
+		CMyistream_iterator<string> inputStr(cin);
+		string s1, s2;
+		s1 = *inputStr;
+		inputStr++;
+		s2 = *inputStr;
+		cout << s1 << " " << s2 << endl;
+	}
+	return 0;
+}
